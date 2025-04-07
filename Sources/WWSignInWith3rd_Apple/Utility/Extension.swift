70378fd27b11179e59f877ac21dbbc890987deae
William.Weng <William.Weng@iit.com.hk>
2025-04-07 10:19:24 +0800

    - public extension WKWebsiteDataStore

--- Sources/WWSignInWith3rd_Apple/Utility/Extension.swift
+++ Sources/WWSignInWith3rd_Apple/Utility/Extension.swift
@@ -138,12 +138,22 @@ public extension WKWebView {
 
 // MARK: - WKWebsiteDataStore (function)
 public extension WKWebsiteDataStore {
+        
+    /// 清除所有的網路記憶
+    /// - Parameters:
+    ///   - date: 從這一天以後的
+    ///   - completion: (Bool) -> Void)?
+    func _clearWebViewMemory(for date: Date = .init(timeIntervalSince1970: 0), completion: ((Bool) -> Void)? = nil) {
+        
+        let allTypes = WKWebsiteDataStore.allWebsiteDataTypes()
+        removeData(ofTypes: allTypes, modifiedSince: date) { completion?(true) }
+    }
     
-    /// [登出 - 清除存在WebView裡面的Cookie值](https://stackoverflow.com/questions/31289838/how-to-delete-wkwebview-cookies)
+    /// [清除特定的網路記憶 - 登出](https://stackoverflow.com/questions/31289838/how-to-delete-wkwebview-cookies)
     /// - Parameters:
     ///   - key: String
     ///   - completion: (Bool)
-    func _cleanWebsiteData(contains key: String, completion: ((Bool) -> Void)?) {
+    func _clearWebViewMemory(contains key: String, completion: ((Bool) -> Void)? = nil) {
         
         let websiteDataTypes = WKWebsiteDataStore.allWebsiteDataTypes()
                 
